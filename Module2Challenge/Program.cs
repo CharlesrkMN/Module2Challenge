ff3aa4c7a9f23b8732ae37fce7ad73eb0866762a
CharlesrkMN <112837649+CharlesrkMN@users.noreply.github.com>
2024-09-09 22:57:57 +0000

    Initial Commit

---
+++ Module2Challenge/Program.cs
@@ -0,0 +1,19 @@
+﻿//declare variables
+int milesDriven;
+int gallonsUsed;
+
+//prompt user for number of miles driven
+Console.Write("Enter the number of miles driven: ");
+//parse to turn value of input into an int
+milesDriven = int.Parse(Console.ReadLine());
+
+//prompt user for amount of gasoline used
+Console.WriteLine("Enter the amount of gasoline used in gallons:");
+//parse to turn value of input into an int
+gallonsUsed = int.Parse(Console.ReadLine());
+
+//calculate MPG (Miles per gallon)
+double mpg = milesDriven / gallonsUsed;
+
+//display mpg to user
+Console.WriteLine($"The miles per gallon is {mpg}");
